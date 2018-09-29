3477eafb668036cc8e0c9b42c8fd55bbf49abcbf
Nolan Dedrickson <git@tesseractworkshop.com>
2018-09-29 11:44:16 -0700

    Add messaging service to log actions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { CodesComponent } from './codes/codes.component';
 import { CodeDetailComponent } from './code-detail/code-detail.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CodesComponent,
-    CodeDetailComponent
+    CodeDetailComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
--- src/app/code.service.ts
+++ src/app/code.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Code } from './code';
 import { MOCK_CODES } from './mock-codes';
 import {Observable, of} from 'rxjs';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +10,10 @@ import {Observable, of} from 'rxjs';
 export class CodeService {
 
   getCodes(): Observable<Code[]> {
+    // TODO:: send the message _after_ fetching codes
+      this.messageService.add('CodeService: Fetched codes');
     return of(MOCK_CODES);
   }
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 }
